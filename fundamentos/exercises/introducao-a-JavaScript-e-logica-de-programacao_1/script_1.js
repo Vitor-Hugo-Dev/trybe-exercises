3f0c990e4de0179e2519663aaf386e2e2e827f39
Vitor Hugo <vitorhugo.dev1997@gmail.com>
2021-05-19 19:14:06 -0300

    ex3

--- fundamentos/exercises/introducao-a-JavaScript-e-logica-de-programacao_1/script_1.js
+++ fundamentos/exercises/introducao-a-JavaScript-e-logica-de-programacao_1/script_1.js
@@ -2,24 +2,28 @@
 let a = 24;
 let b = 18;
 
-let soma = a+b;
+let soma = a + b;
 
-let subtracao = a-b;
+let subtracao = a - b;
 
-let multiplicacao = a*b;
+let multiplicacao = a * b;
 
-let divisao = a/b;
+let divisao = a / b;
 
-let modulo = a%b;
+let modulo = a % b;
 
 // ex2
 
 let c = 10;
 let d = 15;
 
-if (c>d){
+if (c > d) {
     console.log(c);
 }
 else {
     console.log(d);
 }
+// ex3
+
+let e = 20;
+let f = 25; 
