838d943af8989870e3b5face95cdd87da6c07a19
Vitor Hugo <vitorhugo.dev1997@gmail.com>
2021-05-19 23:27:51 -0300

    ex6

--- fundamentos/exercises/introducao-a-JavaScript-e-logica-de-programacao_1/script_1.js
+++ fundamentos/exercises/introducao-a-JavaScript-e-logica-de-programacao_1/script_1.js
@@ -30,13 +30,13 @@ let e = 20;
 let f = 25;
 let g = 30;
 
-if ( e > f && e > g){
+if (e > f && e > g) {
     console.log(e)
 }
-else if ( f > e && f > g){
+else if (f > e && f > g) {
     console.log(f)
 }
-else{
+else {
     console.log(g)
 }
 
@@ -44,13 +44,13 @@ else{
 
 let test4 = 5;
 
-if(test4 > 0){
+if (test4 > 0) {
     console.log("positive");
 }
-else if (test4 < 0){
+else if (test4 < 0) {
     console.log("negative");
 }
-else{
+else {
     console.log("zero");
 }
 
@@ -60,9 +60,42 @@ let h = 80;
 let i = 50;
 let j = 50;
 
-if (h + i + j === 180){
+if (h + i + j === 180) {
     console.log(true);
 }
 else {
     console.log(false);
+}
+// ex6
+
+let k = "Cavalo";
+let kLow = k.toLowerCase();
+
+switch (kLow) {
+    case "rei":
+        console.log("O rei pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.");
+        break;
+
+    case "rainha":
+        console.log("A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças");
+        break;
+
+    case "bispo":
+        console.log("O bispo move-se ao longo da diagonal. Não pode pular outras peças.");
+        break;
+
+    case "cavalo":
+        console.log("É a única peça que pode pular as outras. O movimento do cavalo é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.");
+        break;
+
+    case "torre":
+        console.log("A torre movimenta-se pela vertical ou horizontal, mas não pode pular outras peças.");
+        break;
+
+    case "peão":
+        console.log("O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.");
+        break;
+
+    default:
+        console.log("Essa peça n existe!");
 }
