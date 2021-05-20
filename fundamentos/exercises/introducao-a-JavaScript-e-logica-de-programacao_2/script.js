f38ea8d81857149b574724815ba5f34347ec11cd
Vitor Hugo <vitorhugo.dev1997@gmail.com>
2021-05-20 19:05:13 -0300

    ex2 e 3

--- fundamentos/exercises/introducao-a-JavaScript-e-logica-de-programacao_2/script.js
+++ fundamentos/exercises/introducao-a-JavaScript-e-logica-de-programacao_2/script.js
@@ -1,7 +1,29 @@
 // ex1
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+{
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 0; index < numbers.length; index += 1){
-    console.log(numbers[index]);
+    for (let index = 0; index < numbers.length; index += 1) {
+        console.log(numbers[index]);
+    }
+}
+// ex2
+{
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    let soma = 0;
+    for (let index = 0; index < numbers.length; index += 1) {
+       soma = soma + numbers[index];
+      
+    }
+    console.log(soma);
+}
+// ex3
+{
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    let soma = 0;
+    for (let index = 0; index < numbers.length; index += 1) {
+       soma = soma + numbers[index];
+      
+    }
+    console.log(soma / (numbers.length +=1));
 }
