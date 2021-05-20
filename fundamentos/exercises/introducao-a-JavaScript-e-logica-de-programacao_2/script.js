6504037174ecae0327121a6f7c1aadb14d44f351
Vitor Hugo <vitorhugo.dev1997@gmail.com>
2021-05-20 19:15:18 -0300

    ex4

--- fundamentos/exercises/introducao-a-JavaScript-e-logica-de-programacao_2/script.js
+++ fundamentos/exercises/introducao-a-JavaScript-e-logica-de-programacao_2/script.js
@@ -12,8 +12,8 @@
     let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     let soma = 0;
     for (let index = 0; index < numbers.length; index += 1) {
-       soma = soma + numbers[index];
-      
+        soma = soma + numbers[index];
+
     }
     console.log(soma);
 }
@@ -22,8 +22,26 @@
     let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     let soma = 0;
     for (let index = 0; index < numbers.length; index += 1) {
-       soma = soma + numbers[index];
-      
+        soma = soma + numbers[index];
+
     }
-    console.log(soma / (numbers.length +=1));
+    console.log(soma / (numbers.length += 1));
+}
+// ex4
+{
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    let soma = 0;
+    for (let index = 0; index < numbers.length; index += 1) {
+        soma = soma + numbers[index];
+
+    }
+
+    let media = soma / (numbers.length += 1);
+    if (media > 20) {
+        console.log("valor maior que 20");
+    }
+    else { console.log("valor menor ou igual a 20"); }
+
+
+
 }
