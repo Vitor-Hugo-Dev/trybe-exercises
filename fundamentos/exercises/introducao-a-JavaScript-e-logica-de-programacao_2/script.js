b6ce50cbc22c157f6d8095b7fb50b747bd631f9d
Vitor Hugo <vitorhugo.dev1997@gmail.com>
2021-05-20 20:50:45 -0300

    ex6

--- fundamentos/exercises/introducao-a-JavaScript-e-logica-de-programacao_2/script.js
+++ fundamentos/exercises/introducao-a-JavaScript-e-logica-de-programacao_2/script.js
@@ -48,9 +48,27 @@
     let numMaior = 0;
     for (let index = 0; index < numbers.length; index += 1) {
         // numMaior = numbers[index];
-        if (numbers[index] > numMaior){
+        if (numbers[index] > numMaior) {
             numMaior = numbers[index]
         }
     }
     console.log(numMaior);
 }
+// ex6
+{
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    let numImp = 0;
+    for (let index = 0; index < numbers.length; index += 1) {
+        if ((numbers[index] % 2) !== 0) {
+            numImp += 1;
+        }
+    }
+    if (numImp === 0) {
+        console.log("nenhum valor ímpar encontrado");
+    }
+    else {
+        console.log(numImp);
+    }
+
+}
+// ex7
