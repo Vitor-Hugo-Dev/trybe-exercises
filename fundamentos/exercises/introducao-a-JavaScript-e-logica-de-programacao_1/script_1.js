3ada5b103c80c8cc4e1a5c6d49bc547c25ee275d
Vitor Hugo <vitorhugo.dev1997@gmail.com>
2021-05-20 15:01:05 -0300

    ex7

--- fundamentos/exercises/introducao-a-JavaScript-e-logica-de-programacao_1/script_1.js
+++ fundamentos/exercises/introducao-a-JavaScript-e-logica-de-programacao_1/script_1.js
@@ -99,3 +99,29 @@ switch (kLow) {
     default:
         console.log("Essa peça n existe!");
 }
+
+// ex7
+
+let nota = 40;
+
+if (nota < 0 || nota > 100) {
+    console.log("ERRO")
+}
+else if (nota >= 90) {
+    console.log("A");
+}
+else if (nota >= 80) {
+    console.log("B");
+}
+else if (nota >= 70) {
+    console.log("C");
+}
+else if (nota >= 60) {
+    console.log("D");
+}
+else if (nota >= 50) {
+    console.log("E")
+}
+else {
+    console.log("F");
+}
