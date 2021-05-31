630af3350a7f942e7c13279daf4209af12bc9a0d
Vitor Hugo <vitorhugo.dev1997@gmail.com>
2021-05-31 17:11:18 -0300

    revisao dos exercicios do bloco 5.1

--- fundamentos/exercises/javaScript-Dom-Eventos-e-web-storage_1/exercise_1.js
+++ fundamentos/exercises/javaScript-Dom-Eventos-e-web-storage_1/exercise_1.js
@@ -1,2 +1,26 @@
-const paragraph = document.getElementById("paragraph");
-      paragraph.style.color = "red";
+let titulo = document.getElementById('page-title');
+titulo.style.color = 'blue'
+titulo.innerText = 'FAZENDO RECAPTULAÇÃO EXERCÍCIOS';
+titulo.style.textAlign = 'center';
+titulo.style.fontStyle = 'italic';
+document.body.style.textAlign = 'center';
+
+let secondParagraph = document.getElementById('second-paragraph');
+secondParagraph.innerText = 'ENTENDENDO COMO FUNCIONA OS SELETORES';
+secondParagraph.style.color = 'orange';
+
+let subtitulo = document.getElementById('subtitle');
+subtitulo.style.fontSize = '50px';
+subtitulo.style.color = 'blue';
+subtitulo.innerText += ' parrudo';
+
+let classeIgual = document.getElementsByClassName('igual');
+console.log(classeIgual[0]);
+console.log(classeIgual[1]);
+document.getElementsByClassName('igual')[0].style.fontSize = '50px';
+document.getElementsByClassName('igual')[0].style.color = 'blue';
+
+let tagName = document.getElementsByTagName('h4')[0];
+tagName.style.fontSize = '50px';
+tagName.style.color = 'blue';
+tagName.innerText = ' COLORIDO COM SUCESSO';
