a204eb4292387556467ab51ccfdf8d6bdec90343
Vitor Hugo <vitorhugo.dev1997@gmail.com>
2021-06-05 00:42:42 -0300

    revisao dos exercicios do bloco 5.1

--- fundamentos/exercises/javaScript-Dom-Eventos-e-web-storage_1/exercise_1.js
+++ fundamentos/exercises/javaScript-Dom-Eventos-e-web-storage_1/exercise_1.js
@@ -1,26 +1,26 @@
-let titulo = document.getElementById('page-title');
-titulo.style.color = 'blue'
-titulo.innerText = 'FAZENDO RECAPTULAÇÃO EXERCÍCIOS';
-titulo.style.textAlign = 'center';
-titulo.style.fontStyle = 'italic';
-document.body.style.textAlign = 'center';
+let titulo = document.querySelector("#page-title");
+titulo.style.color = "blue";
+titulo.innerText = "FAZENDO RECAPTULAÇÃO EXERCÍCIOS";
+titulo.style.textAlign = "center";
+titulo.style.fontStyle = "italic";
+document.body.style.textAlign = "center";
 
-let secondParagraph = document.getElementById('second-paragraph');
-secondParagraph.innerText = 'ENTENDENDO COMO FUNCIONA OS SELETORES';
-secondParagraph.style.color = 'orange';
+let secondParagraph = document.querySelector("#second-paragraph");
+secondParagraph.innerText = "ENTENDENDO COMO FUNCIONA OS SELETORES";
+secondParagraph.style.color = "orange";
 
-let subtitulo = document.getElementById('subtitle');
-subtitulo.style.fontSize = '50px';
-subtitulo.style.color = 'blue';
-subtitulo.innerText += ' parrudo';
+let subtitulo = document.querySelector("#subtitle");
+subtitulo.style.fontSize = "50px";
+subtitulo.style.color = "blue";
+subtitulo.innerText += " parrudo";
 
-let classeIgual = document.getElementsByClassName('igual');
+let classeIgual = document.querySelectorAll(".igual");
 console.log(classeIgual[0]);
 console.log(classeIgual[1]);
-document.getElementsByClassName('igual')[0].style.fontSize = '50px';
-document.getElementsByClassName('igual')[0].style.color = 'blue';
+document.getElementsByClassName("igual")[0].style.fontSize = "50px";
+document.getElementsByClassName("igual")[0].style.color = "blue";
 
-let tagName = document.getElementsByTagName('h4')[0];
-tagName.style.fontSize = '50px';
-tagName.style.color = 'blue';
-tagName.innerText = ' COLORIDO COM SUCESSO';
+let tagName = document.querySelectorAll("h4")[0];
+tagName.style.fontSize = "50px";
+tagName.style.color = "blue";
+tagName.innerText = " COLORIDO COM SUCESSO";
---
+++ fundamentos/exercises/javaScript-Dom-Eventos-e-web-storage_1/script_2.js
@@ -0,0 +1,32 @@
+// {
+const container = document.getElementById("container");
+container.style.backgroundColor = "white";
+// }
+// {
+const headerContainer = document.getElementById("header-container");
+headerContainer.style.backgroundColor = "green";
+// }
+// {
+const emergencyTasks = document.getElementsByClassName("emergency-tasks")[0];
+emergencyTasks.style.backgroundColor = "pink";
+const urgImportante = document.getElementsByTagName("h3")[0];
+urgImportante.style.backgroundColor = "purple";
+const urgNaoImportante = document.getElementsByTagName("h3")[1];
+urgNaoImportante.style.backgroundColor = "purple";
+// }
+// {
+const noEmergencyTasks =
+  document.getElementsByClassName("no-emergency-tasks")[0];
+noEmergencyTasks.style.backgroundColor = "yellow";
+const naoUrgImportante = document.getElementsByTagName("h3")[2];
+naoUrgImportante.style.backgroundColor = "black";
+const naoUrgNaoImportante = document.getElementsByTagName("h3")[3];
+naoUrgNaoImportante.style.backgroundColor = "black";
+// }
+// {
+const footerContainer = document.getElementById("footer-container");
+footerContainer.style.backgroundColor = "green";
+
+// }
+
+// esse script é baseado no layout do link: https://app.betrybe.com/course/fundamentals/javascript-dom-eventos-e-web-storage/javascript-dom-e-seletores/d1494833-9ca9-4931-a2cb-75b860813446/conteudos/fdb36e74-011a-46a6-92b4-4bf7854a2b21/parte-iii-qual-seletor-usar/e2850a4a-3387-4769-b614-c38246a46c17?use_case=next_button
