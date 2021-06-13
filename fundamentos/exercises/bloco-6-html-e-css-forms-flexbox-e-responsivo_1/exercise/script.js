d715f7c2425c712de25b567aa322c79c6c184ffe
Vitor Hugo <vitorhugo.dev1997@gmail.com>
2021-06-12 23:52:16 -0300

    conclusao dos exercicios do bloco 6.3

--- fundamentos/exercises/bloco-6-html-e-css-forms-flexbox-e-responsivo_1/exercise/script.js
+++ fundamentos/exercises/bloco-6-html-e-css-forms-flexbox-e-responsivo_1/exercise/script.js
@@ -1,29 +1,47 @@
 const estadosBR = {
-    AC: "Acre",
-    AL: "Alagoas",
-    AP: "Amapá",
-    AM: "Amazonas",
-    BA: "Bahia",
-    CE: "Ceará",
-    DF: "Distrito Federal",
-    ES: "Espírito Santo",
-    GO: "Goías",
-    MA: "Maranhão",
-    MT: "Mato Grosso",
-    MS: "Mato Grosso do Sul",
-    MG: "Minas Gerais",
-    PA: "Pará",
-    PB: "Paraíba",
-    PR: "Paraná",
-    PE: "Pernambuco",
-    PI: "Piauí",
-    RJ: "Rio de Janeiro",
-    RN: "Rio Grande do Norte",
-    RS: "Rio Grande do Sul",
-    RO: "Rondônia",
-    RR: "Roraíma",
-    SC: "Santa Catarina",
-    SP: "São Paulo",
-    SE: "Sergipe",
-    TO: "Tocantins",
-    }; 
+  AC: "Acre",
+  AL: "Alagoas",
+  AP: "Amapá",
+  AM: "Amazonas",
+  BA: "Bahia",
+  CE: "Ceará",
+  DF: "Distrito Federal",
+  ES: "Espírito Santo",
+  GO: "Goiás",
+  MA: "Maranhão",
+  MT: "Mato Grosso",
+  MS: "Mato Grosso do Sul",
+  MG: "Minas Gerais",
+  PA: "Pará",
+  PB: "Paraíba",
+  PR: "Paraná",
+  PE: "Pernambuco",
+  PI: "Piauí",
+  RJ: "Rio de Janeiro",
+  RN: "Rio Grande do Norte",
+  RS: "Rio Grande do Sul",
+  RO: "Rondônia",
+  RR: "Roraíma",
+  SC: "Santa Catarina",
+  SP: "São Paulo",
+  SE: "Sergipe",
+  TO: "Tocantins",
+};
+const pegaSelected = document.getElementById("state-of-people");
+for (let index in estadosBR) {
+  const criaOption = document.createElement("option");
+  criaOption.innerText = estadosBR[index];
+  pegaSelected.appendChild(criaOption);
+}
+
+function interromper (event){
+    event.target.proventDefault()
+}
+document.getElementById('enviar').addEventListener('click', interromper);
+
+
+
+
+// if(document.getElementById('date').value === ""){
+//     alert("você precisa inserir uma data valida!")
+// }
