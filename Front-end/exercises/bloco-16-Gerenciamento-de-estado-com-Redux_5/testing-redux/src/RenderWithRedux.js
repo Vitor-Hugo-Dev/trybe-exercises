9f38aa033a944b527629b8b013c48c6a9db15394
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2021-09-13 18:45:43 -0300

    bloco 16

---
+++ Front-end/exercises/bloco-16-Gerenciamento-de-estado-com-Redux_5/testing-redux/src/RenderWithRedux.js
@@ -0,0 +1,18 @@
+// demais imports...
+import React from 'react'
+import { Provider } from 'react-redux'
+import { render, cleanup } from '@testing-library/react';
+// import App from './App';
+import { createStore, combineReducers } from 'redux';
+import clickReducer from './reducers';
+
+const renderWithRedux = (
+  component,
+  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+) => {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+    store,
+  }
+}
+export default renderWithRedux;
